009053314c44c879ce8c3ba87e3a8c9819e32952
LAPTOP-R2DJIDV0\austi <austin.forsling@gmail.com>
2017-10-26 20:05:29 -0600

    Client socket connects to server. Created UI class

--- src/src/midterm/Client.java
+++ src/src/midterm/Client.java
@@ -1,9 +1,25 @@
 package midterm;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
     
+    private Socket socket;
+    
+    public Client(){
+        try{
+        socket = new Socket("ec2-54-91-0-253.compute-1.amazonaws.com", 8989);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        
+        if (socket.isConnected()){
+            System.out.println("connected");
+        }
+    }
     
     public static void main(String[] args) {
-        
+        new Client();
     }
 }
